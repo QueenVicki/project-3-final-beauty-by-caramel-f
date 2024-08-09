81002eb32c6d6fa460c5e798d302d24dc58b2a56
QueenVicki <Jokilolo9@gmail.com>
2024-08-09 17:13:38 +0100

    added about us page

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,5 +20,6 @@ from home import views as home_views
 
 urlpatterns = [
     path("", home_views.index, name='home'),
+    path("about", home_views.about, name='about'),
     path('admin/', admin.site.urls),
 ]
