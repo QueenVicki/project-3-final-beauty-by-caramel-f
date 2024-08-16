e589ec3b64883b3a272c324b37f6b0a5a3e576ce
QueenVicki <Jokilolo9@gmail.com>
2024-08-16 12:29:56 +0100

    updated migrations

--- home/models.py
+++ home/models.py
@@ -5,3 +5,9 @@ from django.db import models
 class TodoItem(models.Model):
     title = models.CharField (max_length =200)
     completed = models.BooleanField (default =False)
+
+class Customer(models.Model):
+    name = models.CharField (max_length =200, null=True)
+    phone = models.CharField (max_length =200, null=True)
+    email = models.CharField (max_length =200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
